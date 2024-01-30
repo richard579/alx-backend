b18fee534fc93d2efe5ba32a6cb3be76778c2d56
richard579 <richardbeng358@outlook.com>
2024-01-30 21:27:01 +0000

    2. LIFO Caching

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+'''2. LIFO Caching
+'''
+from threading import RLock
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    '''An implementation of LIFO(Last in First out) cache
+    '''
+    def __init__(self):
+        super().__init__()
+        self.__keys = []
+        self.__rlock = RLock()
+
+    def put(self, key, item):
+        '''assign to the dictionary self.cache_data the
+        item value for the key key
+        '''
+        if key is not None and item is not None:
+            keyOut = self._balance(key)
+            with self.__rlock:
+                self.cache_data.update({key: item})
+            if keyOut is not None:
+                print('DISCARD: {}'.format(keyOut))
+
+    def get(self, key):
+        '''return the value in self.cache_data linked to key
+        '''
+        with self.__rlock:
+            return self.cache_data.get(key, None)
+
+    def _balance(self, keyIn):
+        """Removes the oldest item from the cache at MAX size
+        """
+        keyOut = None
+        with self.__rlock:
+            if keyIn not in self.__keys:
+                keysLength = len(self.__keys)
+                if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+                    keyOut = self.__keys.pop(0)
+                    self.cache_data.pop(keyOut)
+            else:
+                self.__keys.remove(keyIn)
+            self.__keys.insert(keysLength, keyIn)
+        return keyOut
